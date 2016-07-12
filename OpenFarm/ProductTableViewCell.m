831fa3d61d308e2abc515168b5f22ef8cf86c544
sally pang <sallyxpang_@live.com>
2016-07-12 09:58:36 -0700

    Product View Cell

---
+++ OpenFarm/ProductTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ProductTableViewCell.m
+//  OpenFarm
+//
+//  Created by sally pang on 2016-07-12.
+//  Copyright © 2016 Sally Pang. All rights reserved.
+//
+
+#import "ProductTableViewCell.h"
+
+@implementation ProductTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
