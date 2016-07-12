ec9359093f3f62834bba1681a897623039a1967e
sally pang <sallyxpang_@live.com>
2016-07-12 10:43:25 -0700

    Assets

--- OpenFarm/ProductTableViewController.m
+++ OpenFarm/ProductTableViewController.m
@@ -40,8 +40,34 @@
 
 - (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     ProductTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ProductCell" forIndexPath:indexPath];
-    cell.productImageView.image = [UIImage imageNamed:@"Apple"];
-    cell.productLabel.text = @"Redlover Era Apple";
+    switch(indexPath.row) {
+        case 0:
+            cell.productImageView.image = [UIImage imageNamed:@"Egg"];
+            cell.productLabel.text = @"Free Run Eggs - Dozen";
+            break;
+        case 1:
+            cell.productImageView.image = [UIImage imageNamed:@"Milk"];
+            cell.productLabel.text = @"2% Dairy Farm Milk";
+            break;
+        case 2:
+            cell.productImageView.image = [UIImage imageNamed:@"Apple"];
+            cell.productLabel.text = @"Redlove Era Apple";
+            break;
+        case 3:
+            cell.productImageView.image = [UIImage imageNamed:@"Orange"];
+            cell.productLabel.text = @"Valencia Oranges - Dozen";
+            break;
+        case 4:
+            cell.productImageView.image = [UIImage imageNamed:@"Lettuce"];
+            cell.productLabel.text = @"Lettuce";
+            break;
+        case 5:
+            cell.productImageView.image = [UIImage imageNamed:@"Tomato"];
+            cell.productLabel.text = @"Better Boy Tomato";
+            break;
+        default:
+            break;
+    }
     
     if([indexPath row] % 2) {
         [cell setBackgroundColor:[UIColor whiteColor]];
---
+++ OpenFarm/ProductViewController.m
@@ -0,0 +1,37 @@
+//
+//  ProductViewController.m
+//  OpenFarm
+//
+//  Created by sally pang on 2016-07-12.
+//  Copyright © 2016 Sally Pang. All rights reserved.
+//
+
+#import "ProductViewController.h"
+
+@interface ProductViewController ()
+
+@end
+
+@implementation ProductViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
